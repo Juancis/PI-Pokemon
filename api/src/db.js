b046963d504deeb44160d0affbb80a72ed3a2781
DESKTOP-7VHGH57\Admin <ioannesgb@gmail.com>
2022-12-02 16:35:06 -0300

    vercel

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+import * as pg from 'pg';
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fnwypxgn`,
   {
+    dialectModule: pg,
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     timestamps: false,
