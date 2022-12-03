4f77be99a4ee4882d5d3d72b181836b9dd360b5c
DESKTOP-7VHGH57\Admin <ioannesgb@gmail.com>
2022-12-03 18:32:38 -0300

    changes reducer

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 import {
   GET_ALL_POKEMONS,
   GET_POKEMON_DETAIL,
@@ -6,66 +6,60 @@ import {
   CLEAR_PAGE,
   GET_ALL_TYPES,
   SET_LOADING,
-  DETAIL_ERROR
-
-} from "../actions/types";
-const URL = process.env.REACT_APP_SERVER + '/pokemons'
-
+  DETAIL_ERROR,
+} from '../actions/types';
+const URL = process.env.REACT_APP_SERVER + '/pokemons';
 
 export const getAllPokemons = () => {
-    return async function (dispatch) {
-        dispatch({type: SET_LOADING})
-        const {data} = await axios.get(URL);
-        return dispatch({type: GET_ALL_POKEMONS, payload: data })
-    }
-}
+  return async function (dispatch) {
+    dispatch({ type: SET_LOADING });
+    const { data } = await axios.get(URL);
+    return dispatch({ type: GET_ALL_POKEMONS, payload: data });
+  };
+};
 
 export const getPokemonDetail = (id) => {
-    return async function (dispatch){
-        dispatch({type: SET_LOADING})
-        try{
-            const {data} = await axios.get(`${URL}/${id}`);
-            return dispatch({type: GET_POKEMON_DETAIL, payload: data})
-        }catch(e){
-            return dispatch({type: DETAIL_ERROR})
-
-        }
-
+  return async function (dispatch) {
+    dispatch({ type: SET_LOADING });
+    try {
+      const { data } = await axios.get(`${URL}/${id}`);
+      return dispatch({ type: GET_POKEMON_DETAIL, payload: data });
+    } catch (e) {
+      return dispatch({ type: DETAIL_ERROR });
     }
-}
+  };
+};
 
 export const getPokemonByName = (name) => {
-    return async function (dispatch) {
-       try{
-        const {data} = await axios.get(`${URL}?name=${name}`)
-        return dispatch({type: GET_POKEMON_BY_NAME, payload: data}) 
-       }catch(e){
-        return e.response.data
-       }  
+  return async function (dispatch) {
+    try {
+      const { data } = await axios.get(`${URL}?name=${name}`);
+      return dispatch({ type: GET_POKEMON_BY_NAME, payload: data });
+    } catch (e) {
+      return e.response.data;
     }
-}
+  };
+};
 
 export const createPokemon = (data) => {
-    return async function() {
-        try {
-            return await axios.post(URL, data).catch()
-        }catch(e){
-            return e.response.data
-        }
+  return async function () {
+    try {
+      return await axios.post(URL, data).catch();
+    } catch (e) {
+      return e.response.data;
     }
-}
+  };
+};
 
 export const clearPage = () => {
-    return function (dispatch){
-        return dispatch({type: CLEAR_PAGE})
-    }
-}
+  return function (dispatch) {
+    return dispatch({ type: CLEAR_PAGE });
+  };
+};
 
-export const getAllTypes = () => { 
-    return async function (dispatch){
-        const {data} = await axios.get(process.env.REACT_APP_SERVER + '/types')
-        return dispatch({type: GET_ALL_TYPES, payload:data})
-
-
-    }
-}
+export const getAllTypes = () => {
+  return async function (dispatch) {
+    const { data } = await axios.get(process.env.REACT_APP_SERVER + '/types');
+    return dispatch({ type: GET_ALL_TYPES, payload: data.slice(0, 20) });
+  };
+};
