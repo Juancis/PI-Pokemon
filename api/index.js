c8bcd324d4faecc1721e6fa00278e0db20137b3c
DESKTOP-7VHGH57\Admin <ioannesgb@gmail.com>
2022-12-03 18:43:18 -0300

    types changes db

--- api/index.js
+++ api/index.js
@@ -26,22 +26,20 @@ const app = server;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
   // CARGO LOS TYPES A LA BD
-  await Type.destroy({
-    truncate: true,
-  });
-
   const { data } = await axios.get(URLtypes);
-  data.results
-    ? Type.bulkCreate(
-        data.results.map((t) => {
-          return {
-            name: t.name,
-          };
-        })
-      )
-    : console.log('Cant charge types');
-  console.log('Types successfully added in database...');
-
+  const types = await Type.findAll();
+  if (types.length === 0) {
+    data.results
+      ? Type.bulkCreate(
+          data.results.map((t) => {
+            return {
+              name: t.name,
+            };
+          })
+        )
+      : console.log('Cant charge types');
+    console.log('Types successfully added in database...');
+  }
   app.listen(process.env.PORT, () => {
     console.log('Server listen...'); // eslint-disable-line no-console
   });
