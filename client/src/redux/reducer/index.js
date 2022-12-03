91bbb7b354b2c69b4aa3afc3beeea07bcefb984f
DESKTOP-7VHGH57\Admin <ioannesgb@gmail.com>
2022-12-03 18:26:52 -0300

    changes reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,48 +1,55 @@
-import * as types from '../actions/types'
+import * as types from '../actions/types';
 
 const initialState = {
-    pokemons: [],
-    types: [],
-    pokemonDetail: {},
-    loading: true,
-}
+  pokemons: [],
+  types: [],
+  pokemonDetail: {},
+  loading: true,
+};
 
 const rootReducer = (state = initialState, action) => {
-    switch(action.type){
-        case types.GET_ALL_POKEMONS:
-            return {
-                ...state, pokemons: action.payload,
-                loading: false
-            }
-        case types.GET_POKEMON_BY_NAME:
-            return {
-                ...state, pokemons: [action.payload]
-            }
-        case types.GET_POKEMON_DETAIL:
-            return {
-                ...state, pokemonDetail: action.payload,
-                loading: false
-            }
-        case types.GET_ALL_TYPES:
-            return {
-                ...state, types: action.payload
-            }
-        case types.CLEAR_PAGE:
-            return {
-                ...state, pokemonDetail: {}
-            }
-        case types.SET_LOADING:
-            return{
-                ...state, loading: true
-            }
-        case types.DETAIL_ERROR:
-            return{
-                ...state, pokemonDetail: 'error',
-                loading: false
-                }   
-            default:
-                return {...state}
-    }
-}
+  switch (action.type) {
+    case types.GET_ALL_POKEMONS:
+      return {
+        ...state,
+        pokemons: action.payload,
+        loading: false,
+      };
+    case types.GET_POKEMON_BY_NAME:
+      return {
+        ...state,
+        pokemons: [action.payload],
+      };
+    case types.GET_POKEMON_DETAIL:
+      return {
+        ...state,
+        pokemonDetail: action.payload,
+        loading: false,
+      };
+    case types.GET_ALL_TYPES:
+      return {
+        ...state,
+        types: action.payload.slice(0, 20),
+      };
+    case types.CLEAR_PAGE:
+      return {
+        ...state,
+        pokemonDetail: {},
+      };
+    case types.SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.DETAIL_ERROR:
+      return {
+        ...state,
+        pokemonDetail: 'error',
+        loading: false,
+      };
+    default:
+      return { ...state };
+  }
+};
 
-export default rootReducer
+export default rootReducer;
